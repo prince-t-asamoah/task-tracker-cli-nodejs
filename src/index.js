c12cbd10bff3535b5cb47f368bf05a689a39de85
Prince Asamoah <prince.t.asamoah@gmail.com>
2025-12-03 14:03:16 +0000

    create and implement main cli function to read cli commands

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 // Application main code goes here
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+function main() {
+  const args = process.argv.slice(2);
+
+  const command = args[0] || ""; // For no command available
+
+  switch (command) {
+    case "help":
+    default:
+
+    // Get the cli instructions
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+      const instructions = fs.readFileSync(
+        path.join(__dirname, "instructions.txt")
+      );
+
+      console.log(instructions.toString());
+  }
+}
+
+// Run main application
+main();
